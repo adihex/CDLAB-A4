a682cfdf87a9c17e8b17dcf573c1160888fb3f4b
adihex <aditya.balakrishnan530@gmail.com>
2023-08-23 10:46:51 +0530

    "Error correction"

--- Section2/qn2.cpp
+++ Section2/qn2.cpp
@@ -1,10 +1,31 @@
 #include <bits/stdc++.h>
-#include <fstream>
+
 using namespace std;
 
+/* Global Data Structures: */
 vector<string> adj[256];
+/* (Helps to generate new symbol as required)
+0 - 25 => A' -> Z'
+26 - 51 -> A'' -> Z''
+*/
+int currSymbol = 0;
 
+string getSymbol() {
+  int base = 'A';
+  int pos = currSymbol % 26;
+  int dashes = currSymbol / 26 + 1;
+  currSymbol++;
+  string resultant;
+  resultant += char(base + pos);
+  while (dashes--)
+    resultant += "'";
+  return resultant;
+}
+
+/** Using Trie Data Structure to do the left refactoring:
+ * -----------------------------------------------------------------*/
 class Trie {
+
 public:
   typedef struct TrieNode {
     TrieNode *child[256];
@@ -31,12 +52,51 @@ public:
       output += symbol + "->";
       for (int i = 0; i < res.size(); i++) {
         output += res[i];
-        if (i != res.size() = 1)
+        if (i != res.size() - 1)
           output += "|";
       }
+      finalResult.push_back(output);
+      return symbol;
     }
+    return "";
+  }
+
+  /** Inserts a word into the trie. */
+  void insert(string word) {
+    TrieNode *temp = head;
+    for (auto x : word) {
+      int pos = x;
+      if (!temp->child[pos]) {
+        TrieNode *created = new TrieNode();
+        temp->child[pos] = created;
+      }
+      temp = temp->child[pos];
+    }
+    temp->isEnd = true;
+  }
+
+  void printTheResultantGrammar(int i) {
+    vector<string> finalResult;
+    string temp;
+    temp += char(i);
+    temp += "->";
+    temp += printHelper(head, finalResult);
+    finalResult.push_back(temp);
+    reverse(finalResult.begin(), finalResult.end());
+    for (auto &x : finalResult)
+      cout << x << endl;
   }
 };
+
+/**--------Helper
+ * Functions-------------------------------------------------------------------------------------------------*/
+
+void printLeftFactoredGrammar(vector<string> s, int i) {
+  Trie *obj = new Trie();
+  for (auto &word : s)
+    obj->insert(word);
+  obj->printTheResultantGrammar(i);
+}
 
 void parseAndInsert(string &str) {
   string removedSpacesFromInput;
@@ -45,7 +105,7 @@ void parseAndInsert(string &str) {
       removedSpacesFromInput += ch;
   str = removedSpacesFromInput;
   if (!(str[0] >= 'A' && str[0] <= 'Z')) {
-    cout << "Error: Non terminals must be uppercase english letters\n";
+    cout << "ERROR: Non terminals must be uppercase english letters";
     exit(-1);
   }
   int index = str[0];
@@ -68,8 +128,13 @@ void parseAndInsert(string &str) {
   return;
 }
 
-int main(void) {
-  ifstream cin("input.txt");
+int main() {
+
+  // These two lines redirects inputs and outputs from/to file:
+  ifstream cin("input2.txt");
+  // ofstream cout("output.txt")
+
+  // Get the input grammar and store it in the global data structure;
   string inputLine;
   while (true) {
     getline(cin, inputLine);
@@ -78,6 +143,12 @@ int main(void) {
     parseAndInsert(inputLine);
   }
 
-  cout << "Left factored grammar:" << endl;
+  cout << "Left Factored Grammar:" << endl;
+  for (int i = 0; i < 256; i++) {
+    if (adj[i].size() == 0)
+      continue;
+    printLeftFactoredGrammar(adj[i], i);
+    cout << endl;
+  }
   return 0;
 }
---
+++ Section3/qn3.cpp
@@ -0,0 +1,104 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// - For question 3,  implement a basic top-down parser for the given grammar
+// using the recursive-descent parsing approach discussed in the class
+/*
+E  -> TE'
+E' -> +TE'|epsilon
+T -> FT'
+T' -> *FT' | epsilon
+F -> (E) | i        // Here i denotes id
+
+Let us model the same grammar by the following transformation: (used for
+simplifying the recursion) E -> 0, E' -> 1, T -> 2, T'-> 3, F -> 4
+
+The updated grammar would be:
+0 -> 21
+1 -> +21 | epsilon
+2 -> 43
+3 -> *43 | epsilon
+4 -> (0) | i
+*/
+
+// Global Data Structure: Representing the current grammar:
+vector<vector<string>> transition{
+    {"21"}, {"+21", ""}, {"43"}, {"*43", ""}, {"(0)", "i"}};
+// For reverse translation:
+unordered_map<int, string> um;
+
+// Recursive Decent Checker for the strings:
+bool isNonTerminal(char x) { return (x >= '0' && x <= '9'); }
+
+string decode(string x) {
+  string result;
+  for (auto ch : x) {
+    if (isNonTerminal(ch))
+      result += um[ch - '0'];
+    else
+      result += ch;
+  }
+  return result;
+}
+
+bool parser(int i, string &s, string &target, int j, vector<string> &sequence,
+            int level) {
+  if (i == s.length() && j == target.length())
+    return true;
+  if (i == s.length())
+    return false;
+  if (!isNonTerminal(s[i])) {
+    if (s[i] == target[j])
+      return parser(i + 1, s, target, j + 1, sequence, level + 1);
+    return false;
+  }
+  int index = s[i] - '0';
+  for (auto x : transition[index]) {
+    string temp = s.substr(0, i) + x + s.substr(i + 1, s.length() - i - 1);
+    if (parser(i, temp, target, j, sequence, level + 1)) {
+      string temp =
+          um[index] + "->" + (x.length() == 0 ? "epsilon" : decode(x));
+      sequence.push_back(temp);
+      return true;
+    }
+  }
+  return false;
+}
+
+int main() {
+  // These two lines redirects inputs and outputs from/to file:
+  ifstream cin("input3.txt");
+  // ofstream cout("output.txt")
+
+  // This is for reverse translation:
+  um[0] = "E";
+  um[1] = "E'";
+  um[2] = "T";
+  um[3] = "T'";
+  um[4] = "F";
+  um[5] = "i";
+
+  // Getting the input:
+  string target, s = "0";
+  cin >> target;
+
+  // Storing the seqence of steps:
+  vector<string> sequence;
+  bool isAccepted = parser(0, s, target, 0, sequence, 0);
+
+  if (isAccepted)
+    cout << "Accepted: ";
+  else
+    cout << "Rejected: ";
+  cout << target << endl;
+
+  if (isAccepted) {
+    cout << "The leftmost derivation is shown below:" << endl;
+    reverse(sequence.begin(), sequence.end());
+    for (auto &x : sequence)
+      cout << x << endl;
+  }
+
+  return 0;
+}
